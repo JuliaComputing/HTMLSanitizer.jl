dbd4a00b26c2b16a41416ffeb3414e90a823457a
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2020-04-29 17:08:15 +0200

    gumbo 0.8 compat

--- test/malicious_html.jl
+++ test/malicious_html.jl
@@ -23,42 +23,42 @@
 
   @testset "<img>" begin
     @testset "should not be possible to inject JS via an unquoted <img> src attribute" begin
-      @test "<img></img>" == HTMLSanitizer.sanitize("""<img src=javascript:alert('XSS')>""")
+      @test "<img/>" == HTMLSanitizer.sanitize("""<img src=javascript:alert('XSS')>""")
     end
 
     @testset "should not be possible to inject JS using grave accents as <img> src delimiters" begin
-      @test "<img></img>" == HTMLSanitizer.sanitize("""<img src=`javascript:alert('XSS')`>""")
+      @test "<img/>" == HTMLSanitizer.sanitize("""<img src=`javascript:alert('XSS')`>""")
     end
 
     @testset "should not be possible to inject <script> via a malformed <img> tag" begin
-      @test """<img></img>">""" == HTMLSanitizer.sanitize("""<img \"\"\"><script>alert("XSS")</script>">""")
+      @test """<img/>\"&gt;""" == HTMLSanitizer.sanitize("""<img \"\"\"><script>alert("XSS")</script>">""")
     end
 
     @testset "should not be possible to inject protocol-based JS" begin
-      @test "<img></img>" == HTMLSanitizer.sanitize("""<img src=&#106;&#97;&#118;&#97;&#115;&#99;&#114;&#105;&#112;&#116;&#58;&#97;&#108;&#101;&#114;&#116;&#40;&#39;&#88;&#83;&#83;&#39;&#41;>""")
+      @test "<img/>" == HTMLSanitizer.sanitize("""<img src=&#106;&#97;&#118;&#97;&#115;&#99;&#114;&#105;&#112;&#116;&#58;&#97;&#108;&#101;&#114;&#116;&#40;&#39;&#88;&#83;&#83;&#39;&#41;>""")
 
-      @test "<img></img>" == HTMLSanitizer.sanitize("""<img src=&#0000106&#0000097&#0000118&#0000097&#0000115&#0000099&#0000114&#0000105&#0000112&#0000116&#0000058&#0000097&#0000108&#0000101&#0000114&#0000116&#0000040&#0000039&#0000088&#0000083&#0000083&#0000039&#0000041>""")
+      @test "<img/>" == HTMLSanitizer.sanitize("""<img src=&#0000106&#0000097&#0000118&#0000097&#0000115&#0000099&#0000114&#0000105&#0000112&#0000116&#0000058&#0000097&#0000108&#0000101&#0000114&#0000116&#0000040&#0000039&#0000088&#0000083&#0000083&#0000039&#0000041>""")
 
-      @test "<img></img>" == HTMLSanitizer.sanitize("""<img src=&#x6A&#x61&#x76&#x61&#x73&#x63&#x72&#x69&#x70&#x74&#x3A&#x61&#x6C&#x65&#x72&#x74&#x28&#x27&#x58&#x53&#x53&#x27&#x29>""")
+      @test "<img/>" == HTMLSanitizer.sanitize("""<img src=&#x6A&#x61&#x76&#x61&#x73&#x63&#x72&#x69&#x70&#x74&#x3A&#x61&#x6C&#x65&#x72&#x74&#x28&#x27&#x58&#x53&#x53&#x27&#x29>""")
 
       # Encoded tab character.
-      @test "<img></img>" == HTMLSanitizer.sanitize("""<img src="jav&#x09;ascript:alert('XSS');">""")
+      @test "<img/>" == HTMLSanitizer.sanitize("""<img src="jav&#x09;ascript:alert('XSS');">""")
 
       # Encoded newline.
-      @test "<img></img>" == HTMLSanitizer.sanitize("""<img src="jav&#x0A;ascript:alert('XSS');">""")
+      @test "<img/>" == HTMLSanitizer.sanitize("""<img src="jav&#x0A;ascript:alert('XSS');">""")
 
       # Encoded carriage return.
-      @test "<img></img>" == HTMLSanitizer.sanitize("""<img src="jav&#x0D;ascript:alert('XSS');">""")
+      @test "<img/>" == HTMLSanitizer.sanitize("""<img src="jav&#x0D;ascript:alert('XSS');">""")
 
       # Spaces plus meta char.
-      @test "<img></img>" == HTMLSanitizer.sanitize("""<img src=" &#14;  javascript:alert('XSS');">""")
+      @test "<img/>" == HTMLSanitizer.sanitize("""<img src=" &#14;  javascript:alert('XSS');">""")
 
       # Mixed spaces and tabs.
-      @test "<img></img>" == HTMLSanitizer.sanitize("""<img src="j\na v\tascript://alert('XSS');">""")
+      @test "<img/>" == HTMLSanitizer.sanitize("""<img src="j\na v\tascript://alert('XSS');">""")
     end
 
     @testset "should not be possible to inject protocol-based JS via whitespace" begin
-      @test "<img></img>" == HTMLSanitizer.sanitize("""<img src="jav\tascript:alert('XSS');">""")
+      @test "<img/>" == HTMLSanitizer.sanitize("""<img src="jav\tascript:alert('XSS');">""")
     end
 
     @testset "should not be possible to inject JS using a half-open <img> tag" begin
